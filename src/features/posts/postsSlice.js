df856ce2e34d95f47080881c53041700733b742f
Xillyk <xillyk@hotmail.com>
2023-04-19 01:09:21 +0700

    fix reactionAdded reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -57,14 +57,14 @@ const postsSlice = createSlice({
         };
       },
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
   },
-  reactionAdded(state, action) {
-    const { postId, reaction } = action.payload
-    const existingPost = state.find(post => post.id === postId)
-    if (existingPost) {
-      existingPost.reactions[reaction]++
-    }
-  }
 });
 
 export const selectAllPosts = (state) => state.posts;
