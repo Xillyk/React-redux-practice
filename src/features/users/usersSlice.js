763f186fe852699a3f3b3bd5cb7a4370fcfe3c1e
Xillyk <xillyk@hotmail.com>
2023-04-21 00:12:18 +0700

    implement async get user in usersSlice

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,24 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {
-    id: "0",
-    name: "Boba Cocoa",
-  },
-  {
-    id: "1",
-    name: "David Dance",
-  },
-  {
-    id: "2",
-    name: "Amar Star",
-  },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
