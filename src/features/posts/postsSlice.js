df913e2adadec6da31a4123cb9d11d5e47c9e4f8
Xillyk <xillyk@hotmail.com>
2023-04-23 19:16:45 +0700

    add updatePost

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -20,6 +20,16 @@ export const addNewPost = createAsyncThunk("posts/addNewPost", async (initialPos
   return response.data;
 });
 
+export const updatePost = createAsyncThunk("/posts/updatePost", async (initialPost) => {
+  const { id } = initialPost;
+  try {
+    const response = await axios.put(`${POST_URL}/${id}`, initialPost);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
+
 const postsSlice = createSlice({
   name: "posts",
   initialState,
@@ -96,6 +106,17 @@ const postsSlice = createSlice({
         };
         console.log(action.payload);
         state.posts.push(action.payload);
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Update could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = [...posts, action.payload];
       });
   },
 });
