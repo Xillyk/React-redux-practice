a6ccd81f4f5efd5abe89a8b31bde4a4ec01bf4b2
Xillyk <xillyk@hotmail.com>
2023-04-23 20:33:36 +0700

    refactor fetchPost to fetch on init

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
+import { fetchPosts } from "./features/posts/postsSlice";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
